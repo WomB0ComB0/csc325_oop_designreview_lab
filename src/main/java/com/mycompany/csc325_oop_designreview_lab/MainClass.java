802d54b0b8db2611affcef027d13b5bad252995b
Mike Odnis <mikeodnis3242004@gmail.com>
2024-06-25 20:16:27 -0400

    initial commit, stripped history

---
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.csc325_oop_designreview_lab;
+
+/**
+ *
+ * @author MoaathAlrajab
+ */
+public class MainClass {
+
+ public static void main(String[] args) {
+		// ToDo 5: Fix the error
+
+		// ToDo 6: Fix the constructor of the Student class
+
+                // Todo 7: Create two classes for Freshman and Senior 
+
+                // ToDo 8: The senior class should have a minimum of 85 credits  
+
+		// ToDo 9: Add a toString method for the Student class
+		// ToDo 10: Add a toString method for the Freshman class
+
+		Student std1= new Student("James", 20);
+                // ToDo 11: Add a toString method for the Senior class
+
+		Freshman std1= new Student("James", 20, 12); // name, age, credits
+
+                Senior std2 = new Student("John", 30, 90);
+
+		// ToDo 12: Set the gpa of the student using the scanner and user
+		// 			input and then print the output.
+
+		System.out.println(std1);
+
+                System.out.println(std2);
+
+		// ToDo 13: add comments and explain your code
+
+	}
+
+}
+
